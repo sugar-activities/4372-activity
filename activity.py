720ebf5d72bdc57a44cef03953183b6182b8841f
Ignacio Rodriguez <ignacio@sugarlabs.org>
2017-04-21 01:33:01 -0400

    Initial commit

---
+++ activity.py
@@ -0,0 +1,9 @@
+from olpcgames import activity
+from gettext import gettext as _
+
+class Activity(activity.PyGameActivity):
+    """Your Sugar activity"""
+    
+    game_name = 'conozcoact'
+    game_title = _('Conozco Actividades')
+    game_size = (1200,900)
